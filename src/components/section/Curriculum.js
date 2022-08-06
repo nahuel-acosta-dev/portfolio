48d7262c2ab9f893b0f242fbecc7965e4dcb2bbe
Nahuel Acosta <brianacostanahuel2000@gmail.com>
2022-08-06 19:57:59 -0300

    Basic section of curriculum section finished

--- src/components/section/Curriculum.js
+++ src/components/section/Curriculum.js
@@ -1,6 +1,7 @@
 import React, {useEffect,useState} from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Card from 'react-bootstrap/Card';
 import Accordion from 'react-bootstrap/Accordion';
 import One from '../../image/certificates/one.png';
 import Harvard from '../../image/certificates/harvard.svg';
@@ -9,6 +10,13 @@ import CodoACodo from '../../image/certificates/logocodoacodo.png';
 
 const Curriculum = ({children}) =>{
     const [certificates, setCertificates] = useState();
+    const information = [
+        {title: 'Certificates', knowledge: "Front-end developer"},
+        {title: 'Certificates', knowledge: "Front-end developer"},
+        {title: 'Certificates', knowledge: "Front-end developer"},
+        {title: 'Certificates', knowledge: "Front-end developer"},
+        {title: 'Certificates', knowledge: "Front-end developer"}
+    ];
 
     const getCertificates = async () => {
         const response = await fetch("certificates.json")
@@ -44,9 +52,9 @@ const Curriculum = ({children}) =>{
 
     return(
         <section className="curriculum">
-            <Row className="justify-content-center">
-                <Col md={1}></Col>
-                <Col className="curriculum__texts text-center">
+            <Row className="justify-content-md-center">
+                <Col xs={1}></Col>
+                <Col className="curriculum__texts text-center justify-content-center">
                     <div className="curriculum__title text-center">
                         {children}
                     </div>
@@ -57,20 +65,20 @@ const Curriculum = ({children}) =>{
                         typeface without relying on meaningful content.
                     </div>
                 </Col>
-                <Col></Col>
+                <Col xs={1} md></Col>
             </Row>
             <Row>
                 <Col xs={1}></Col>
-                <Col xs={10} md={7}>
+                <Col xs={10} md={6}>
                     <Accordion className="curriculum__diplomature">
                         {certificates &&
                             certificates.map((certificate,i) => (
-                            <Accordion.Item eventKey={i} key={i}>
+                            <Accordion.Item eventKey={i} key={i} className="curriculum__accordion">
                                 <Accordion.Header>
                                     <div className="curriculum__imgs">
                                         <img 
                                             src={getImg(certificate.img)}
-                                            width="30px"
+                                            width="20px"
                                             alt="icon Html"
                                         />
                                     </div>
@@ -83,7 +91,7 @@ const Curriculum = ({children}) =>{
                                             certificate.subs.map((sub,i) => (
                                                 <Col xs={12} md key={`sub-${i}`}>
                                                 <Accordion className="curriculum__sub">
-                                                <Accordion.Item eventKey="0">
+                                                <Accordion.Item eventKey="0" className="curriculum__accordion">
                                                 <Accordion.Header>
                                                     <div  className="curriculum__imgs--sub">
                                                         <img 
@@ -112,8 +120,29 @@ const Curriculum = ({children}) =>{
                         }
                     </Accordion>
                 </Col>
-                <Col xs={10} md={3}>
-                    
+                <Col xs={10} md={4}>
+                    <Row>
+                    <Col xs={2}></Col>
+                    <Col>
+                    {
+                        information.map((info, i) => (
+                            <Card key={i} className="curriculum__info">
+                                <Card.Body className="text-center">
+                                    <Card.Subtitle className="mb-2 text-muted">
+                                        {info.title}
+                                    </Card.Subtitle>
+                                    <Card.Text>
+                                        <strong>
+                                            {info.knowledge}
+                                        </strong>
+                                    </Card.Text>
+                                </Card.Body>
+                            </Card>
+
+                        ))
+                    }
+                    </Col>
+                    </Row>
                 </Col>
                 <Col xs={1}></Col>
             </Row>
--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
 import 'bootstrap/dist/js/bootstrap.bundle.min';
 import './App.scss';
+import './index.css';
 import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
