5d50e4425181951f65dcef754a475b64e8dc3cd7
Nahuel Acosta <brianacostanahuel2000@gmail.com>
2022-08-11 20:05:49 -0300

    skills section card design change

--- src/sections/Skills.js
+++ src/sections/Skills.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
+import Container from 'react-bootstrap/Container';
 import iconHtml from '../image/icons/html.svg';
 import iconCss from '../image/icons/css.svg';
 import iconJavascript from '../image/icons/javascript.svg';
@@ -14,108 +15,56 @@ const Skills = ({children}) =>{
 
     return(
         <section className="skills" id="skills">
-            <div>
-                <Row>
-                    <Col xs={1}></Col>
-                    <Col className="skills__texts text-center">
-                        <div className="skills__title">
-                            {children}
-                        </div>
-                        <div className="skills__text">
-                            In publishing and graphic design, 
-                            Lorem ipsum is a placeholder text commonly 
-                            used to demonstrate the visual form of a document or a 
-                            typeface without relying on meaningful content.
-                        </div>
-                    </Col>
-                    <Col xs={1} sm></Col>
-                </Row>
-                <Row className="justify-content-center">
-                    <Col md={1}></Col>
-                    <Col xs={10} md className="skills__front">
-                        <p>DESARROLLO FRONT-END</p>
-                        <p>
-                            I have extensive knowledge of HTML, CSS and JavaScript (ES6), 
-                            experience with the 3 most popular frameworks (React,Vue and Angular) 
-                            and TypeScript. Add a lot of curiosity, creativity and a GTD-mentality 
-                            and you have a pretty complete picture of me.
-                        </p>
-                        <Row>
-                            <Col>
-                                <img 
-                                    src={iconHtml}
-                                    width="50px"
-                                    alt="icon Html"
-                                />
-                            </Col>
-                            <Col>
-                                <img 
-                                    src={iconCss}
-                                    width="50px"
-                                    alt="icon Css"
-                                />
-                            </Col>
-                            <Col>
-                                <img 
-                                src={iconJavascript}
-                                width="50px"
-                                alt="icon Javascript"
-                                />
-                            </Col>
-                                <Col>
-                                    <img 
-                                        src={iconBootstrap}
-                                        width="50px"
-                                        alt="icon Bootstrap"
+            <div className="bg"></div>
+            <div className="bg2"></div>
+            <Container>
+                <Row className="skills__cont align-self-center align-justify-content-center">
+                        <Col sm={12} md></Col>
+                        <Col sm={12} md>
+                            <ul className="skills__dev">
+                                <li>
+                                    <Row>
+                                        <Col xs={2}>
+                                            <img src={iconPython} 
+                                            alt="icon python"
+                                            width="50"
+                                            />
+                                        </Col>
+                                        <Col>
+                                            <span>
+                                                <strong>AppCoins</strong>
+                                                <p>In publishing and graphic design, 
+                                                Lorem ipsum is a placeholder </p>
+                                            </span>
+                                        </Col>
+                                    </Row>
+                                </li>
+                                <li>
+                                    <img src={iconDjango} 
+                                    alt="icon django"
+                                    width="50"
                                     />
-                                </Col>
-                                <Col>
-                                    <img 
-                                    src={iconReact}
-                                    width="50px"
-                                    alt="icon React"
+                                    <span>
+                                        <strong>AppCoins</strong>
+                                        <p>In publishing and graphic design, 
+                                        Lorem ipsum is a placeholder </p>
+                                    </span>
+                                </li>
+                                <li>
+                                    <img src={iconMysql} 
+                                    alt="icon Mysql"
+                                    width="50"
                                     />
-                                </Col>
-                        </Row>
-                    </Col>
-                    <Col md={1}></Col>
-                    <Col xs={10} md>
-                        <p>DESARROLLO BACK-END</p>
-                        <p>
-                        I speak the designer mumbo-jumbo and dare to make design decisions while coding. 
-                        This saves valuable development time, endless ping-ponging with designers and 
-                        often looks even better than the original design. I also do not hesitate to work 
-                        out something in Figma or Sketch myself.
-                        </p>
-                        <Row>
-                            <Col md={1}></Col>
-                            <Col>
-                            <img 
-                                src={iconPython}
-                                width="50px"
-                                alt="icon Html"
-                            />
-                            </Col>
-                            <Col>
-                            <img 
-                                src={iconDjango}
-                                width="50px"
-                                alt="icon Html"
-                            />
-                            </Col>
-                            <Col>
-                            <img 
-                                src={iconMysql}
-                                width="50px"
-                                alt="icon Html"
-                            />
-                            </Col>
-                            <Col md={3}></Col>
-                        </Row>
-                    </Col>
-                    <Col md={1}></Col>
-                </Row>
-            </div>
+                                    <span>
+                                        <strong>AppCoins</strong>
+                                        <p>In publishing and graphic design, 
+                                        Lorem ipsum is a placeholder </p>
+                                    </span>
+                                </li>
+                            </ul>
+                        </Col>
+                    </Row>
+            </Container>
         </section>
     )
 }
