7f9e4e631917496d781b395b84f9751c72e00fa6
Nahuel Acosta <brianacostanahuel2000@gmail.com>
2022-08-13 20:35:23 -0300

    desgin finished in contact section

--- src/sections/Contact.js
+++ src/sections/Contact.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
+import Astronauta from '../image/astronauta.png';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
 
@@ -11,35 +12,38 @@ const Contact = () =>{
         <section className="contact" id="contact">
             <Container>
                 <Row>
-                    <Col xs={1}/>
+                    <Col lg={1}/>
                     <Col>
                         <Row>
-                            <Col className="contact__text">
-                                <p>Get in Touch</p>
-                                <p>Contact me</p>
+                            <Col xs={12} md>
+                                <div className="contact__cont">
+                                    <img src={Astronauta} alt="astronauta Imagen"/>
+                                </div>
                             </Col>
-                            <Col>
+                            <Col className="contact__form" xs={12} md>
+                                <p className="contact__text">Ponerse en Contacto</p>
                                 <Form>
                                     <Form.Group className="mb-3" controlId="exampleForm.ControlInput1">
                                         <Form.Label>Nombre</Form.Label>
-                                        <Form.Control className="contact__input" type="email" placeholder="Nombre" />
+                                        <Form.Control className="contact__input" type="text" placeholder="Nombre" />
                                     </Form.Group>
                                     <Form.Group className="mb-3" controlId="exampleForm.ControlInput2">
                                         <Form.Label>Email</Form.Label>
-                                        <Form.Control className="contact__input" type="email" placeholder="email" />
+                                        <Form.Control className="contact__input" type="email" placeholder="Email" />
                                     </Form.Group>
                                     <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
                                         <Form.Label>Mensaje</Form.Label>
-                                        <Form.Control className="contact__input" as="textarea" rows={3} placeholder="Mensaje" />
+                                        <Form.Control className="contact__input" as="textarea" rows={3} 
+                                        placeholder="Mensaje" />
                                     </Form.Group>
-                                    <Button variant="warning" type="submit">
-                                        enviar
-                                    </Button>
+                                        <Button className="contact__btn" variant="dark" size="lg" type="submit">
+                                            <span>enviar</span>
+                                        </Button>
                                     </Form>
                             </Col>
                         </Row>
                     </Col>
-                    <Col xs={2}/>
+                    <Col lg={2}/>
                 </Row>
             </Container>
         </section>
